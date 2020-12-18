13ce1091f3fb4d618d438799565f9c65a25af06f
erikvinicius <erik.vinicius9911@gmail.com>
2020-12-18 13:35:20 -0300

    first commit

---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
+
+class DeleteTransactionService {
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) throw new AppError('Transaction not found!', 404);
+
+    await transactionsRepository.remove(transaction);
+    // TODO
+  }
+}
+
+export default DeleteTransactionService;
